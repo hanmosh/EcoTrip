eb1bef4ebf37fabdee4e7f92ac3138176e34c572
Shafay Syed <ssyed10@calpoly.edu>
2023-04-22 16:35:13 -0700

    Form changes

--- milestone-3/my-app/frontend/src/components/App/form.js
+++ milestone-3/my-app/frontend/src/components/App/form.js
@@ -9,26 +9,58 @@ function form() {
 
    setTimeout(() => {
      setSubmitting(false);
-   }, 3000)
+   }, 3000);
  }
-
+ const handleChange = event => {
+    const isCheckbox = event.target.type === 'checkbox';
+    setFormData({
+      name: event.target.name,
+      value: isCheckbox ? event.target.checked : event.target.value,
+    })
+  }
   return(
     <div className="wrapper">
-      <h1>Who's the goat? (its'lebron)</h1>
+      <h1>Who's the Goat? (its lebron) </h1>
       {submitting &&
-       <div>Submtting Form...</div>
-     }
+        <div>
+          You are submitting the following:
+          <ul>
+            {Object.entries(formData).map(([name, value]) => (
+              <li key={name}><strong>{name}</strong>: {value.toString()}</li>
+            ))}
+          </ul>
+        </div>
+      }
       <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
             <p>Name</p>
-            <input name="name" />
+            <input name="name" onChange={handleChange}/>
           </label>
         </fieldset>
+        <fieldset>
+         <label>
+           <p>Lebron</p>
+           <select name="apple" onChange={handleChange}>
+               <option value="">--Please choose an option--</option>
+               <option value="fuji">Jordan</option>
+               <option value="jonathan">Melo</option>
+               <option value="honey-crisp">KD</option>
+           </select>
+         </label>
+         <label>
+           <p>Count</p>
+           <input type="number" name="count" onChange={handleChange} step="1"/>
+         </label>
+         <label>
+           <p>Gift Wrap</p>
+           <input type="checkbox" name="gift-wrap" onChange={handleChange} />
+         </label>
+       </fieldset>
         <button type="submit">Submit</button>
       </form>
     </div>
   )
 }
 
-export default App;
+export default form;
