856b5545928e425e0767002c4850aa352b4f636b
Shafay Syed <ssyed10@calpoly.edu>
2023-04-22 16:44:15 -0700

    Back to og form

--- milestone-3/my-app/frontend/src/components/App/form.js
+++ milestone-3/my-app/frontend/src/components/App/form.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './form.css';
 
-function form() {
+function Form() {
   const [submitting, setSubmitting] = useState(false);
   const handleSubmit = event => {
     event.preventDefault();
@@ -9,58 +9,26 @@ function form() {
 
    setTimeout(() => {
      setSubmitting(false);
-   }, 3000);
+   }, 3000)
  }
- const handleChange = event => {
-    const isCheckbox = event.target.type === 'checkbox';
-    setFormData({
-      name: event.target.name,
-      value: isCheckbox ? event.target.checked : event.target.value,
-    })
-  }
+
   return(
     <div className="wrapper">
-      <h1>Who's the Goat? (its lebron) </h1>
+      <h1>Who's the goat? (its'lebron)</h1>
       {submitting &&
-        <div>
-          You are submitting the following:
-          <ul>
-            {Object.entries(formData).map(([name, value]) => (
-              <li key={name}><strong>{name}</strong>: {value.toString()}</li>
-            ))}
-          </ul>
-        </div>
-      }
+       <div>Submtting Form...</div>
+     }
       <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
             <p>Name</p>
-            <input name="name" onChange={handleChange}/>
+            <input name="name" />
           </label>
         </fieldset>
-        <fieldset>
-         <label>
-           <p>Lebron</p>
-           <select name="apple" onChange={handleChange}>
-               <option value="">--Please choose an option--</option>
-               <option value="fuji">Jordan</option>
-               <option value="jonathan">Melo</option>
-               <option value="honey-crisp">KD</option>
-           </select>
-         </label>
-         <label>
-           <p>Count</p>
-           <input type="number" name="count" onChange={handleChange} step="1"/>
-         </label>
-         <label>
-           <p>Gift Wrap</p>
-           <input type="checkbox" name="gift-wrap" onChange={handleChange} />
-         </label>
-       </fieldset>
         <button type="submit">Submit</button>
       </form>
     </div>
   )
 }
 
-export default form;
+export default Form;
