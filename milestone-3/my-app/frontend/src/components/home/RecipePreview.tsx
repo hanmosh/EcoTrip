793f2b67722191c30e3ebd7560a25a2c596f0d77
Noah Ramones <g.o.a.t@Noahs-MacBook-Pro-4.local>
2023-04-23 07:14:06 -0700

    welcome page and how you can help

--- milestone-3/my-app/frontend/src/components/home/RecipePreview.tsx
+++ milestone-3/my-app/frontend/src/components/home/RecipePreview.tsx
@@ -19,7 +19,11 @@ function RecipePreview (props: Recipe){
                     </Link>
                     
                 </div>
-                <p style={{ position: "absolute", top: "10%", left: "50%", transform: "translate(-50%, -50%)" }}><span>Welcome</span> to a Eco-Friendly Tomorrow.</p>
+                <div style={{ position: "absolute", top: "30%", left: "50%", transform: "translate(-50%, -50%)", textAlign: "center" }}>
+                    <p className="welcome-class" style={{ fontSize: "60px", fontWeight: "bold", fontFamily: "'Roboto Slab', seriff", margin: 0 }}>Welcome</p>
+                    <p style={{ fontSize: "35px", fontFamily: "'Roboto Slab', serif", margin: 0 }}>to a Eco-Friendly Tomorrow.</p>
+                </div>
+
             </div>
         </div>
     )
