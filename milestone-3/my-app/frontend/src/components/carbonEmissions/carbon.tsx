0969e779b7e9f13821c63c35e873516480752053
hannah <113932134+hanmosh@users.noreply.github.com>
2023-04-23 07:31:09 -0700

    carbon graph and css changes

--- milestone-3/my-app/frontend/src/components/carbonEmissions/carbon.tsx
+++ milestone-3/my-app/frontend/src/components/carbonEmissions/carbon.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import './carbon.css';
+import Graph from './Screen Shot 2023-04-23 at 7.05.42 AM.png';
+
 
 export default function Carbon() {
   return(
-<p>graph here</p>
+    
+    <div className="Graph">
+      <a href="https://replit.com/@rajaadil07/GranularAllAccounting?from=notifications#index.html" target="_blank">       
+       <img src= {Graph} alt= "carbon emissions graph" height="700" width="900"/>
+      </a>
+    </div>
+
   );
 }
