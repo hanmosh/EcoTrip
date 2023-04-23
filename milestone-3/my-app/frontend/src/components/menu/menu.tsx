7c4b0d33e496fa37e882d4f7e1f14a872dc81839
Noah Ramones <g.o.a.t@Noahs-MacBook-Pro-4.local>
2023-04-23 00:56:33 -0700

    Homepage background image

--- milestone-3/my-app/frontend/src/components/menu/menu.tsx
+++ milestone-3/my-app/frontend/src/components/menu/menu.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from 'react-router-dom';
 
 export default function Menu() {
@@ -6,21 +6,15 @@ export default function Menu() {
     <div className="clickable-images">
 
       <Link to='/form'>
-        <a>
-          <img src= "https://cdn-icons-png.flaticon.com/512/2988/2988198.png" alt= "calculator" height="400" width="400"/>
-        </a>
+        <img src="https://cdn-icons-png.flaticon.com/512/2988/2988198.png" alt="calculator" height="400" width="400" />
       </Link>
-      
+
       <Link to='/form'>
-        <a>
-          <img src= "https://www.pngplay.com/wp-content/uploads/6/Airplane-Transparent-PNG.png" alt= "plane" height="400" width="400"/>
-        </a>
+        <img src="https://www.pngplay.com/wp-content/uploads/6/Airplane-Transparent-PNG.png" alt="plane" height="400" width="400" />
       </Link>
-      
+
       <Link to='/form'>
-        <a>
-          <img src= "https://starpng.com/public/uploads/preview/handshake-transparent-png-image-101577197248lxk7urbwp2.png" alt= "handshake" height="400" width="400"/>
-        </a>
+        <img src="https://starpng.com/public/uploads/preview/handshake-transparent-png-image-101577197248lxk7urbwp2.png" alt="handshake" height="400" width="400" />
       </Link>
 
     </div>
