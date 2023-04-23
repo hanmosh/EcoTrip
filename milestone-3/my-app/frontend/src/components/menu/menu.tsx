d895063b9fb22d91a4c38523f753ca1e88f2a50f
hannah <113932134+hanmosh@users.noreply.github.com>
2023-04-22 20:17:14 -0700

    minor changes

--- milestone-3/my-app/frontend/src/components/menu/menu.tsx
+++ milestone-3/my-app/frontend/src/components/menu/menu.tsx
@@ -4,24 +4,25 @@ import { Link } from 'react-router-dom';
 export default function Menu() {
   return (
     <div className="clickable-images">
+
       <Link to='/form'>
         <a>
           <img src= "https://cdn-icons-png.flaticon.com/512/2988/2988198.png" alt= "calculator" height="400" width="400"/>
         </a>
       </Link>
-
+      
       <Link to='/form'>
         <a>
           <img src= "https://www.pngplay.com/wp-content/uploads/6/Airplane-Transparent-PNG.png" alt= "plane" height="400" width="400"/>
         </a>
       </Link>
-
+      
       <Link to='/form'>
         <a>
           <img src= "https://starpng.com/public/uploads/preview/handshake-transparent-png-image-101577197248lxk7urbwp2.png" alt= "handshake" height="400" width="400"/>
         </a>
       </Link>
-    
+
     </div>
   );
 }
